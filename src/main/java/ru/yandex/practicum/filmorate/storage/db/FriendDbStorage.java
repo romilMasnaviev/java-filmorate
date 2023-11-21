3761b42354e264ce5d5cfccde876e221113fcf5e
Romil Masnaviev <romil.masnavievv@yandex.ru>
2023-11-21 09:00:48 +0300

    review 1.0

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/FriendDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/FriendDbStorage.java
@@ -29,33 +29,20 @@ public class FriendDbStorage implements FriendStorage {
         String sqlDelete = "DELETE FROM friends WHERE user_id = ? AND friend_id = ?";
         jdbcTemplate.update(sqlDelete, id, friendId);
     }
+
     private List<User> processFriendsQuery(String sql, Object[] params) {
-        return jdbcTemplate.query(sql, params, (rs, rowNum) -> User.builder()
-                .id(rs.getInt("user_id"))
-                .email(rs.getString("user_email"))
-                .login(rs.getString("user_login"))
-                .name(rs.getString("user_name"))
-                .birthday(rs.getDate("user_birthday").toLocalDate())
-                .friendsId(getFriendsIds(rs.getInt("user_id")))
-                .build());
+        return jdbcTemplate.query(sql, params, (rs, rowNum) -> User.builder().id(rs.getInt("user_id")).email(rs.getString("user_email")).login(rs.getString("user_login")).name(rs.getString("user_name")).birthday(rs.getDate("user_birthday").toLocalDate()).friendsId(getFriendsIds(rs.getInt("user_id"))).build());
     }
 
     @Override
     public List<User> getFriendsList(int id) {
-        String sqlGetId = "SELECT u.user_id, u.user_email, u.user_login, u.user_name, u.user_birthday " +
-                "FROM users u " +
-                "JOIN friends f ON u.user_id = f.friend_id " +
-                "WHERE f.user_id = ?";
+        String sqlGetId = "SELECT u.user_id, u.user_email, u.user_login, u.user_name, u.user_birthday " + "FROM users u " + "JOIN friends f ON u.user_id = f.friend_id " + "WHERE f.user_id = ?";
         return processFriendsQuery(sqlGetId, new Object[]{id});
     }
 
     @Override
     public List<User> getSameFriends(int id, int otherId) {
-        String sqlGetSame = "SELECT u.user_id, u.user_email, u.user_login, u.user_name, u.user_birthday " +
-                "FROM users u " +
-                "JOIN friends f1 ON u.user_id = f1.friend_id " +
-                "JOIN friends f2 ON f1.friend_id = f2.friend_id " +
-                "WHERE f1.user_id = ? AND f2.user_id = ?";
+        String sqlGetSame = "SELECT u.user_id, u.user_email, u.user_login, u.user_name, u.user_birthday " + "FROM users u " + "JOIN friends f1 ON u.user_id = f1.friend_id " + "JOIN friends f2 ON f1.friend_id = f2.friend_id " + "WHERE f1.user_id = ? AND f2.user_id = ?";
         return processFriendsQuery(sqlGetSame, new Object[]{id, otherId});
     }
 
